87c3d1b052e431cd82f89168fb6f4ca1c0931f75
0-harshit-0 <harshitw3b@gmail.com>
2024-12-24 20:14:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,25 +19,3 @@ function clsBrightness() {
 }
 
 export { clsNightMode, clsBrightness };
-
-// export default EyeComfortComponent({
-//     title="", className="", handleClick=()=>{}, content="Eye Comfort",
-//     element=document.querySelector(":root"), defaultFilter=""
-// }) {
-//     let enabled = false;
-
-//     const handleEyeComfortBtnClick = (e) => {
-//         handleClick();
-
-//         !enabled && element.style.setProperty("filter", "brightness(.7) sepia(1)");
-//         enabled && element.style.setProperty("filter", defaultFilter);
-
-//         enabled = !enabled;
-//     }
-
-//     return(
-//         <button title={title} className={className} onClick={handleEyeComfortBtnClick}>
-//             {content}
-//         </button>
-//     );
-// }
